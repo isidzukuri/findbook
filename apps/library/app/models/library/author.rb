32051e072338d2edf01508723aef1bbb10863812
isidzukuri <axesigon@gmail.com>
2019-09-18 16:45:08 +0300

    validates seo

--- apps/library/app/models/library/author.rb
+++ apps/library/app/models/library/author.rb
@@ -3,5 +3,7 @@ module Library
     has_and_belongs_to_many :books
 
     validates :full_name, presence: true
+    validates :seo, presence: true
+    validates_uniqueness_of :seo
   end
 end
--- apps/library/app/models/library/book.rb
+++ apps/library/app/models/library/book.rb
@@ -3,5 +3,7 @@ module Library
     has_and_belongs_to_many :authors
 
     validates :title, presence: true
+    validates :seo, presence: true
+    validates_uniqueness_of :seo
   end
 end
