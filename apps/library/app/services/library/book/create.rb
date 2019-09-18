21df7eaeaa7a469395caf73e7776bd067728c5a8
isidzukuri <axesigon@gmail.com>
2019-09-18 21:45:43 +0300

    book create service

---
+++ apps/library/app/services/library/book/create.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Library
+  module Book
+    class Create < ::SimpleTools::Operation
+      step :build
+      step :generate_slug
+      step :validate
+      step :persist
+      # TODO:
+      # step :add_authors
+      # step :add_to_elastic
+      # step :store_cover
+
+      def build
+        book = Book.new(params[:book])
+
+        update_context(:book, book)
+      end
+
+      def generate_slug
+        result = Seo::GenerateSlug.call(object: not_uniq_book, attribute: :title)
+        context[:book].seo = result.context[:slug]
+      end
+
+      def validate
+        errors!(context[:book].errors.messages) unless context[:book].validate
+      end
+
+      def persist
+        errors!(context[:book].errors.messages) unless context[:book].save
+      end
+    end
+  end
+end
